689202fa8d5041eff5abf2d49d444c6ad299c341
XFFXFF <1247714429@qq.com>
2020-09-12 18:23:51 +0800

    feat: 添加concurrent_queue的简单测试用例

--- test/common/concurrent_queue_test.cpp
+++ test/common/concurrent_queue_test.cpp
@@ -2,6 +2,23 @@
 #include "gtest/gtest.h"
 #include <iostream>
 
-int main() {
-  std::cout << "hello gtest" << std::endl;
+namespace noisepage
+{
+class ConcurrentQueueTests : public ::testing::Test {};
+
+TEST_F(ConcurrentQueueTests, SimpleTest) {
+  const int max_queue_size = 100;
+  ConcurrentQueue<int> queue;
+  for (int i = 0; i < max_queue_size; i++) {
+    queue.Enqueue(std::move(i));
+  }
+  EXPECT_EQ(queue.UnsafeSize(), max_queue_size);
+  int dest;
+  for (int i = 0; i < max_queue_size; i++) {
+    queue.Dequeue(dest);
+    EXPECT_EQ(dest, i);
+  }
+  EXPECT_EQ(queue.UnsafeSize(), 0);
 }
+
+} // namespace noisepage
