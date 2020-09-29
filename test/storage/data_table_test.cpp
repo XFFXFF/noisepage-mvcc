a555fbfe8bf7093dd0d4f7a9a8c4c9bf348d2c40
XFFXFF <1247714429@qq.com>
2020-09-29 08:35:38 +0800

    refactor: RandomDataTableTestObject

--- test/storage/data_table_test.cpp
+++ test/storage/data_table_test.cpp
@@ -6,9 +6,10 @@
 namespace noisepage {
 class RandomDataTableTestObject {
 public:
-  RandomDataTableTestObject(const storage::BlockLayout &layout,
-                            storage::DataTable &data_table, const double null_bias) 
-                            : layout_(layout), data_table_(data_table), null_bias_(null_bias) {}
+  template<typename Random>
+  RandomDataTableTestObject(storage::BlockStore &store, uint32_t max_col, double null_bias, Random &generator) 
+                            : layout_(testutil::RandomLayout(generator, max_col)), 
+                              data_table_(store, layout_), null_bias_(null_bias) {}
   
   ~RandomDataTableTestObject() {
     for (auto *ptr : loose_pointers_) {
@@ -46,9 +47,13 @@ public:
     assert(inserted_rows_map_.find(slot) != inserted_rows_map_.end());
     return inserted_rows_map_[slot];
   }
+
+  const storage::BlockLayout &Layout() const {
+    return layout_;
+  }
 private:
   const storage::BlockLayout layout_;
-  storage::DataTable &data_table_;
+  storage::DataTable data_table_;
   const double null_bias_;
   std::vector<byte *> loose_pointers_;
   std::unordered_map<storage::TupleSlot, storage::ProjectedRow *> inserted_rows_map_;
@@ -59,29 +64,28 @@ private:
   byte *select_buffer_ = new byte[redo_size_];
 };
   
-class DataTableTests : public ::testing::Test {};
+struct DataTableTests : public ::testing::Test {
+  storage::BlockStore block_store_{10};
+  std::default_random_engine generator_;
+  std::uniform_real_distribution<double> null_ratio_{0.0, 1.0};
+};
 
 TEST_F(DataTableTests, SimpleTest) {
-  const uint32_t max_col = 100;
   const uint32_t repeat = 10;
-  std::default_random_engine generator;
+  const uint32_t max_col = 100;
 
   for (uint32_t i = 0; i < repeat; i++) {
-    storage::BlockStore block_store(10);
-    storage::BlockLayout layout = testutil::RandomLayout(generator, max_col); 
-    storage::DataTable data_table(block_store, layout);
-    std::uniform_real_distribution<double> null_ratio{0.0, 1.0};
-    RandomDataTableTestObject tested(layout, data_table, null_ratio(generator));
+    RandomDataTableTestObject tested(block_store_, max_col, null_ratio_(generator_), generator_);
     std::vector<storage::TupleSlot> inserted_slots_;
     
     for (uint32_t j = 0; j < 10; j++) {
-      auto slot = tested.InsertRandomTuple(generator);
+      auto slot = tested.InsertRandomTuple(generator_);
       inserted_slots_.push_back(slot);
     }
 
     for (auto slot : inserted_slots_) {
       auto *select_row = tested.SelectIntoBuffer(slot);
-      EXPECT_TRUE(testutil::ProjectionListEqual(layout, *select_row, 
+      EXPECT_TRUE(testutil::ProjectionListEqual(tested.Layout(), *select_row, 
                                                 *tested.GetInsertedRow(slot)));
     }
 
